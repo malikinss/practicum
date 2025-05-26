1f485c2aca42eeabf7fbb435e6d00d2ad6f5ccc7
malikinss <workmalikin@gmail.com>
2025-05-27 02:28:27 +0300

    Add overview and structure documentation for modules directory
    - Described purpose and integration of modules: record, calculator, cash_calculator, calories_calculator.
    - Outlined how modules support financial and dietary tracking.
    - Documented intended use as part of a larger project with potential test integration.

---
+++ python_engineer_plus/sprint_2/2_8_final_project/2_8_1_money_calorie_calculator/src/modules/__init__.py
@@ -0,0 +1,11 @@
+from .calculator import Calculator
+from .calories_calculator import CaloriesCalculator
+from .cash_calculator import CashCalculator
+from .record import Record
+
+__all__ = ["Calculator", "CashCalculator", "CaloriesCalculator", "Record"]
+__version__ = '1.0.0'
+__author__ = 'Sam Malikin'
+__email__ = 'shepwoks@gmail.com'
+__license__ = 'MIT License'
+__status__ = 'Development'
